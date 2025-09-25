887867994c8f1c75e72fb6218bf0e79b47457edc
Guilherme Henrique de Souza <guilherme.souza@etec.sp.gov.br>
2025-09-25 14:17:46 -0300

    25/9

---
+++ exercicio1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Exercício de PHP</h1>
+
+    <form action="exercicio1a.php">
+
+        <p>
+            Digite o nome do cliente <br>
+            <input type="text" name="cliente">
+        </p>
+
+        <p>
+            Escolha o sexo do cliente <br>
+            <input type="radio" name="sexo" value="Feminino"> Feminino <br>
+            <input type="radio" name="sexo" value="Masculino">Masculino
+        </p>
+
+        <p>
+            Digite o valor da compra <br>
+            <input type="number" name="valorcompra">
+        </p>
+
+        <p>
+            <input type="submit" value="Enviar">
+        </p>
+
+    </form>
+
+
+</body>
+</html>
